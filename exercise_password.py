870ed523e88fe739dff51c6bc721de30d73db2ed
jpguille <jpguillen2008@gmail.com>
2026-03-31 11:18:11 -0300

    Update exercise_password.py

--- exercise_password.py
+++ exercise_password.py
@@ -23,4 +23,18 @@ def password():
         Contraseña muy corta
         Debe contener un numero
     """
-    pass
+
+    
+    contrasena = input()
+    numeros = '0' in contrasena or '1' in contrasena or '2' in contrasena or '3' in contrasena or '4' in contrasena or '5' in contrasena or '6' in contrasena or '7' in contrasena or '8' in contrasena or '9' in contrasena
+    no_numeros = '0' not in contrasena and '1' not in contrasena and '2' not in contrasena and '3' not in contrasena and '4' not in contrasena and '5' not in contrasena and '6' not in contrasena and '7' not in contrasena and '8' not in contrasena and '9' not in contrasena
+
+    if len(contrasena) >= 8 and numeros:
+        print('Contraseña valida')
+    elif len(contrasena) < 8 and numeros:
+        print('Contraseña muy corta')
+    elif len(contrasena) >= 8 and no_numeros:
+        print('Debe contener un numero')
+    elif len(contrasena) < 8 and no_numeros:
+        print('''Contraseña muy corta
+Debe contener un numero''')
